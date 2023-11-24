ea6b467e5fe9b1df908769b0b465f597334c6dc6
Chareeef <youssef.charif.h@gmail.com>
2023-11-24 10:49:52 -0800

    Add 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# Install flask from pip3
-
-package { 'python3-pip' :
-  ensure  => installed,
-}
-package { 'Flask':
+# Install Flask v2.1.0
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
