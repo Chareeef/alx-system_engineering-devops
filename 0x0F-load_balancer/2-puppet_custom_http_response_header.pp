8ffd53a587a7a91615cef3bcd037cc87fe089940
Chareeef <youssef.charif.h@gmail.com>
2023-12-04 17:54:57 +0000

    Add 2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Set up an nginx server so it has a custom HTTPN response header
+
+exec { 'Update apt':
+  command  => 'sudo apt-get update',
+  provider => shell,
+  before   => Exec['Nginx'],
+}
+
+exec { 'Nginx':
+  command  => 'sudo apt-get install nginx -y',
+  provider => shell,
+  before   => Exec['Header'],
+}
+
+exec { 'Header':
+  command  => 'sudo sed -i s#server_name _;#server_name _;\n\tadd_header X-Served-By $(hostname);# /etc/nginx/sites-available/default',
+  provider => shell,
+  before   => Exec['Restart'],
+}
+
+exec { 'Restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
