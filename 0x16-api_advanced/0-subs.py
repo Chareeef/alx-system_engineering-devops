aeeb06eb3a8f8a87898855d6c04a2fa6843f88b6
Chareeef <youssef.charif.h@gmail.com>
2024-01-16 12:32:24 +0100

    Reddit number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to query the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Query the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Youssef'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
+        return 0
